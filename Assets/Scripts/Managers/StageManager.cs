7bcabf8e18b9c88bd3ca476f607b22401a00bea3
Hyomly <hm_lee09@naver.com>
2025-02-14 00:00:46 +0900

    Update
    Build

--- Assets/Scripts/Managers/StageManager.cs
+++ Assets/Scripts/Managers/StageManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.AI.Navigation;
-using UnityEditor.Playables;
 using UnityEngine;
 
 public class StageManager : SingletonDontDestroy<StageManager>
